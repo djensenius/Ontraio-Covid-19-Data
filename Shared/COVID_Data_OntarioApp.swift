b85cfbe3c0addc44c3d9a8eea9fb6a2809b95265
David Jensenius <david@jensenius.com>
2021-01-02 16:50:53 -0500

    Initial Commit

---
+++ Shared/COVID_Data_OntarioApp.swift
@@ -0,0 +1,17 @@
+//
+//  COVID_Data_OntarioApp.swift
+//  Shared
+//
+//  Created by David Jensenius on 2021-01-02.
+//
+
+import SwiftUI
+
+@main
+struct COVID_Data_OntarioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
